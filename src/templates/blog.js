6dff6f1681e4c8190378474f00bc839510a63a7d
Jurek <jurek2006@gmail.com>
2019-08-08 08:07:34 +0200

    14. Rendering Post Data in Blog Template - post html

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -20,6 +20,9 @@ const Blog = props => {
     <Layout>
       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
       <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      ></div>
     </Layout>
   )
 }
