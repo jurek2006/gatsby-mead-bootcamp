18ffd2b8b9e86f9980953bdd1232ce18b199faa5
Jurek <jurek2006@gmail.com>
2019-08-08 07:16:50 +0200

    14. Rendering Post Data in Blog Template - error - cannot read markdownRemark

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,27 @@
 import React from "react"
+import { graphql } from "gatsby"
+
 import Layout from "../components/layout"
 
-const Blog = () => {
-  return <Layout>This is the blog template.</Layout>
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+    </Layout>
+  )
 }
 
 export default Blog
